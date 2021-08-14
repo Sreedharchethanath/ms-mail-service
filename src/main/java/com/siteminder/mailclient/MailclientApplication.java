33ad96cf70f28a5a418232da247cdc89de7046d2
sreedhar s <sreedhars966@gmail.com>
2021-08-14 19:38:12 +1000

    Commiting initial version

---
+++ src/main/java/com/siteminder/mailclient/MailclientApplication.java
@@ -0,0 +1,13 @@
+package com.siteminder.mailclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siteminder/mailclient/MailclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siteminder.mailclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
